854e3639bb2acc575765b66a15b65ecfb0d87d3e
Şamil Korkmaz <samil.korkmaz@gmail.com>
2017-07-16 20:45:29 +0300

    Added tests for ArgumentOutOfRangeException

--- TestSerialPort/TestChecksumControl.cs
+++ TestSerialPort/TestChecksumControl.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Text;
 using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -12,6 +11,7 @@ namespace TestSerialPort
     public class TestChecksumControl
     {
         MySerialPort.Crc32 crc32 = new MySerialPort.Crc32();
+
         [TestMethod]
         public void TestIsChecksumOk1()
         {
@@ -20,10 +20,57 @@ namespace TestSerialPort
             byte[] checksum = { 0x21, 0x44, 0xDF, 0x1C };
             receivedList.Add(checksum);
             bool isOK = MySerialPort.ChecksumControl.isChecksumOk(receivedList, crc32, 4);
-            String str = String.Format("Received = {0}, Expected = {1}", 
+            Assert.IsTrue(isOK);
+        }
+
+        [TestMethod]
+        public void TestIsChecksumOk2()
+        {
+            List<byte[]> receivedList = new List<byte[]>();
+            receivedList.Add(new byte[] { 0xE9, 0x5C, 0xA8, 0xEE, 0xFF, 0x8D, 0xFA, 0xD9, 0xC4, 0xEA });
+            byte[] checksum = { 0xBF, 0x54, 0xE3, 0x63 };
+            receivedList.Add(checksum);
+            bool isOK = MySerialPort.ChecksumControl.isChecksumOk(receivedList, crc32, 4);
+            String str = String.Format("Received = {0}, Expected = {1}",
                 MySerialPort.ChecksumControl.getByteArrayAsString(MySerialPort.ChecksumControl.Received),
                 MySerialPort.ChecksumControl.getByteArrayAsString(MySerialPort.ChecksumControl.Expected));
             Assert.IsTrue(isOK);
+        }
+
+        [TestMethod]
+        public void TestIsChecksumOk3()
+        {
+            try
+            {
+                List<byte[]> receivedList = new List<byte[]>();
+                receivedList.Add(new byte[] { });
+                byte[] checksum = { 0xBF, 0x54, 0xE3, 0x63 };
+                receivedList.Add(checksum);
+                bool isOK = MySerialPort.ChecksumControl.isChecksumOk(receivedList, crc32, 4);
+                Assert.IsTrue(false);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Assert.IsTrue(true);
+            }
+        }
+
+        [TestMethod]
+        public void TestIsChecksumOk4()
+        {
+            try
+            {
+                List<byte[]> receivedList = new List<byte[]>();
+                receivedList.Add(new byte[] { });
+                byte[] checksum = { };
+                receivedList.Add(checksum);
+                bool isOK = MySerialPort.ChecksumControl.isChecksumOk(receivedList, crc32, 4);
+                Assert.IsTrue(false);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Assert.IsTrue(true);
+            }
         }
     }
 }
